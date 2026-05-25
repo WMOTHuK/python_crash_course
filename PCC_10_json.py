6710f999ec79b37b460cbc217142e4e3a0607d22
Konstantin Alimov <alimovshared@gmail.com>
2026-05-25 22:32:41 +0300

    PCC_chaper 10, task 10-13

--- PCC_10_json.py
+++ PCC_10_json.py
@@ -1,6 +1,6 @@
 """ Python Crash Course Chapter 10 (JSON)"""
 #standard imports
-
+import json
 #local imports
 from PCC_input_functions import getuserint
 from PCC_json_functions import (write_value_to_json,
@@ -26,3 +26,35 @@ else:
     write_value_to_json(filename, fav_num)
 
 
+# 10-13 User check
+    
+def get_new_username():
+    """Asks and returns username"""
+    prompt = "Please enter your name: \n"
+    return(input(prompt))
+
+def greet_user(username):
+    print(f'Hello, {username}!! Nice to see you!!')
+
+def add_new_user(filename):
+    username = get_new_username()
+    write_value_to_json(filename, username)
+    greet_user(username)
+
+#Initial Data
+filename = 'username.json'
+
+username = read_value_from_json(filename)
+
+if username is None:
+    add_new_user(filename)
+else:
+    question = ("Hey,"  + username + "! Is that you?" + " y/n \n")
+    answer = input(question)
+    if answer == 'y':
+        greet_user(username)
+    else:
+        add_new_user(filename)
+
+
+     
